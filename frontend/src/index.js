f7d30b0c416298373c0849910ab6cd99caf91a46
cam4994 <colin.a.mosley@gmail.com>
2020-09-07 23:02:33 -0500

    Created maze function and added create user and login forms

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,130 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    console.log("Hello World")
+document.addEventListener("DOMContentLoaded", () => {
+    createMaze()
+    function createMaze(){
+
+        let maze;
+
+        function generate(dimensions) {
+            maze = new Array();
+            for (let i = 0; i < dimensions; i++) {
+                maze[i] = new Array();
+
+                for (let j = 0; j < dimensions; j++) {
+                    maze[i][j] = "";
+                }
+            }
+
+            addOuterWalls();
+            addInnerWalls(true, 1, maze.length - 2, 1, maze.length - 2);
+            addEntrance();
+            addExit()
+        }
+
+        function addOuterWalls() {
+            for (let i = 0; i < maze.length; i++) {
+                if (i == 0 || i == (maze.length - 1)) {
+                    // Top and Bottom Borders
+                    for (let j = 0; j < maze.length; j++) {
+                        maze[i][j] = "w";
+                    }
+                } else {
+                    // Left and Right Borders
+                    maze[i][0] = "w";
+                    maze[i][maze.length - 1] = "w";
+                }
+            }
+        }
+
+        function addEntrance() {
+            let x = 0;
+            //Check to see if there is an open space above entrance, if not move over one space
+            for (let i = 1; i < maze.length - 1; i++) {
+                if (maze[maze.length - 2][i] == "") {
+                    x = i;
+                    break;
+                }
+            }
+            maze[maze.length - 1][x] = "g";
+        }
+
+        function addExit() {
+            let x = 0;
+            for (let i = maze.length-1; i > 0; i--) {
+                if (maze[1][i] == "") {
+                    x = i; 
+                    break;
+                }
+            }
+            maze[0][x] = "e"
+        }
+        function addInnerWalls(h, minX, maxX, minY, maxY) {
+            if (h) {
+
+                if (maxX - minX < 2) {
+                    return;
+                }
+                //walls only in even cells
+                let y = Math.floor(randomNumber(minY, maxY)/2)*2;
+                if (y == 0){
+                    y = 2;
+                }
+                addHWall(minX, maxX, y);
+
+                addInnerWalls(!h, minX, maxX, minY, y - 1);
+                addInnerWalls(!h, minX, maxX, y + 1, maxY);
+            } else {
+                if (maxY - minY < 2) {
+                    return;
+                }
+                //walls only in even cells
+                let x = Math.floor(randomNumber(minX, maxX)/2)*2;
+                if (x == 0){
+                    x = 2;
+                }
+                addVWall(minY, maxY, x);
+
+                addInnerWalls(!h, minX, x - 1, minY, maxY);
+                addInnerWalls(!h, x + 1, maxX, minY, maxY);
+            }
+        }
+
+        function addHWall(minX, maxX, y) {
+            //doors in odd cells
+            let hole = Math.floor(randomNumber(minX, maxX) / 2) * 2 + 1;
+
+            for (let i = minX; i <= maxX; i++) {
+                if (i == hole) maze[y][i] = "";
+                else maze[y][i] = "w";
+            }
+        }
+
+        function addVWall(minY, maxY, x) {
+            //doors in odd cells
+            let hole = Math.floor(randomNumber(minY, maxY) / 2) * 2 + 1;
+
+            for (let i = minY; i <= maxY; i++) {
+                if (i == hole) maze[i][x] = "";
+                else maze[i][x] = "w";
+            }
+        }
+
+        function randomNumber(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
+
+        function display() {
+            document.getElementById("maze").innerHTML = "";
+
+            for (let i = 0; i < maze.length; i++) {
+                let output = "<div>";
+                for (let j = 0; j < maze.length; j++) {
+                    output += "<b " + maze[i][j] + "></b>";
+                }
+                output += "</div>";
+                document.getElementById("maze").innerHTML += output;
+            }
+        }
+        generate(71);
+        display();
+    }
 })
---
+++ frontend/src/maze.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let createForm = document.querySelector('.user-form')
+    let logInForm = document.querySelector('.login-form')
+
+    createForm.addEventListener('submit', (e)=>{
+        createOrLoginUser(e, "http://localhost:3000/users")
+    })
+    logInForm.addEventListener('submit', (e)=>{
+        createOrLoginUser(e, "http://localhost:3000/login")
+    })
+
+    function createOrLoginUser(event, url) {
+        event.preventDefault()
+        console.log(event.target)
+        console.log(url)
+        let username = event.target.username.value
+        let password = event.target.password.value
+        event.target.username.value = ''
+        event.target.password.value = ''
+
+        let configObj = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                "username": username,
+                "password": password
+            })
+        }
+    
+        fetch(url, configObj)
+            .then(resp => resp.json())
+            .then(user=> console.log(user))
+    }
+})
